ec2fa5d193d82ec5620d813b20b03a8db34ea89a
Alexandra Sokolets <alsun@ya.ru>
2023-11-08 17:48:08 +0300

    First release

---
+++ js/chunk-7470e963.6ae4ebf2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7470e963"],{"2fb4":function(n,w,o){}}]);
+//# sourceMappingURL=chunk-7470e963.6ae4ebf2.js.map
---
+++ js/chunk-7485d655.82bc0c96.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7485d655"],{"57c5":function(n,w,c){}}]);
+//# sourceMappingURL=chunk-7485d655.82bc0c96.js.map
